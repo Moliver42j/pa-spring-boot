6496e7def34a01999c455bd08d9bdfe11aa3be88
Matthew Oliver <50474630+Moliver42j@users.noreply.github.com>
2019-09-20 16:21:14 +0100

    Done for friday

--- 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/TopTenMusic/service/TopTenService.java
+++ 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/TopTenMusic/service/TopTenService.java
@@ -17,19 +17,19 @@ public class TopTenService {
 		this.topSongs = new ArrayList<TopTen>();
 
 		// TopTen(position, artistName, song, genre, image)
-		this.topSongs.add(new TopTen("1", "Post Malone", "Hollywood's Bleeding", "rap/hiphop", "postmalonesong.png"));
-		this.topSongs.add(new TopTen("2", "Taylor Swift", "Lover", "rap/hiphop", "taylorswiftsong.png"));
-		this.topSongs.add(new TopTen("3", "Melanie Martinez", "k-12", "rap/hiphop", "melaniemartinezsong.png"));
-		this.topSongs.add(new TopTen("4", "Young Thug", "So Much Fun", "rap/hiphop", "youngthugsong.png"));
-		this.topSongs.add(new TopTen("5", "Lil Tecca", "We love you Tecca", "rap/hiphop", "lilteccasong.png"));
-		this.topSongs.add(new TopTen("6", "Lizzo", "Cuz I love you", "rap/hiphop", "lizzosong.png"));
-		this.topSongs.add(new TopTen("7", "Tool", "Fear Inoculum", "rap/hiphop", "toolsong.png"));
-		this.topSongs.add(new TopTen("8", "Billie Eilish", "When We All Fall Asleep, Where do we Go?", "rap/hiphop",
+		this.topSongs.add(new TopTen("1", "Post Malone",      "Hollywood's Bleeding", "Rap/Hip-hop", "postmalonesong.png"));
+		this.topSongs.add(new TopTen("2", "Taylor Swift", 	  "Lover", "Pop", "taylorswiftsong.png"));
+		this.topSongs.add(new TopTen("3", "Melanie Martinez", "k-12", "Unkown \n (Didn't listen to it)", "melaniemartinezsong.png"));
+		this.topSongs.add(new TopTen("4", "Young Thug",       "So Much Fun", "Rap/Hip-hop", "youngthugsong.png"));
+		this.topSongs.add(new TopTen("5", "Lil Tecca",        "We love you Tecca", "Rap/Hip-hop", "lilteccasong.png"));
+		this.topSongs.add(new TopTen("6", "Lizzo",            "Cuz I love you", "Unkown \n (Didn't listen to it)", "lizzosong.png"));
+		this.topSongs.add(new TopTen("7", "Tool",             "Fear Inoculum", "rap/hiphop", "toolsong.png"));
+		this.topSongs.add(new TopTen("8", "Billie Eilish",    "When We All Fall Asleep, Where do we Go?", "Pop",
 				"billieeilishsong.png"));
 		this.topSongs
-				.add(new TopTen("9", "Lana Del Ray", "Norman F***ing Rockwell!", "rap/hiphop", "lanadelraysong.png"));
+				.add(new TopTen("9", "Lana Del Ray", "Norman F***ing Rockwell!", "Pop (I assume)", "lanadelraysong.png"));
 		this.topSongs
-				.add(new TopTen("10", "The Highwaywomen", "The Highwaywomen", "rap/hiphop", "thehighwaywomensong.png"));
+				.add(new TopTen("10", "The Highwaywomen", "The Highwaywomen", "Pop (I assume)", "thehighwaywomensong.png"));
 
 		return this.topSongs;
 	}
@@ -38,15 +38,15 @@ public class TopTenService {
 		this.topArtists = new ArrayList<TopTen>();
 
 		// TopTen(artistName, song, position, genre, image)
-		this.topArtists.add(new TopTen("1","Post Malone", "Beerbongs and Bentleys", "rap/hiphop", "PostMaloneartist.png"));
-		this.topArtists.add(new TopTen("2","Taylor Swift", "Me", "rap/hiphop", "taylorswiftartist.png"));
-		this.topArtists.add(new TopTen("3","Billie Eilish", "Ocean Eyes", "rap/hiphop", "billieeilishartist.png"));
-		this.topArtists.add(new TopTen("4","Lizzo", "Truth Hurts", "rap/hiphop", "lizzoartist.png"));
-		this.topArtists.add(new TopTen("5","Khalid", "Talk","rap/hiphop", "Khalidartist.png"));
+		this.topArtists.add(new TopTen("1","Post Malone", "Beerbongs and Bentleys", "Rap/Hip-hop", "PostMaloneartist.png"));
+		this.topArtists.add(new TopTen("2","Taylor Swift", "Me", "Pop", "taylorswiftartist.png"));
+		this.topArtists.add(new TopTen("3","Billie Eilish", "Ocean Eyes", "Pop", "billieeilishartist.png"));
+		this.topArtists.add(new TopTen("4","Lizzo", "Truth Hurts", "Unkown \n (Didn't listen to it)", "lizzoartist.png"));
+		this.topArtists.add(new TopTen("5","Khalid", "Talk","R&B or something", "Khalidartist.png"));
 		this.topArtists.add(new TopTen("6","Tool", "Schism","rap/hiphop", "Toolartist.png"));
-		this.topArtists.add(new TopTen("7","Ed Sheeran", "Shape Of You", "rap/hiphop","edsheeranartist.png"));
-		this.topArtists.add(new TopTen("8","Camila Cabello", "Havana","rap/hiphop", "camilacabelloartist.png"));
-		this.topArtists.add(new TopTen("9","Melanie Martinez", "Too Close","rap/hiphop", "melaniemartinezartist.png"));
+		this.topArtists.add(new TopTen("7","Ed Sheeran", "Shape Of You", "Pop","edsheeranartist.png"));
+		this.topArtists.add(new TopTen("8","Camila Cabello", "Havana","Pop", "camilacabelloartist.png"));
+		this.topArtists.add(new TopTen("9","Melanie Martinez", "Too Close","Unkown \n (Didn't listen to it)", "melaniemartinezartist.png"));
 
 		return this.topArtists;
 	}
